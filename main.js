960c761d2542e2359de4808c98474b9b777ba9a6
MFvanRossum <matissev@hotmail.com>
2020-06-26 12:22:40 -0400

    8-Track modal opens and closes

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById('overlay')
+
+// openModalButtons.forEach(div => {
+//     div.addEventListener('click', () => {
+//         const modal = document.querySelector(div.dataset.modalTarget)
+//         openModal(modal)
+//     })
+// })
+
+// closeModalButtons.forEach(div => {
+//     div.addEventListener('click', () => {
+//         const modal = i.closest('.modal')
+//         closeModal(modal)
+//     })
+// })
+
+// function openModal(modal) {
+//     if (modal == null) return
+//     modal.classList.add('active')
+//     overlay.classList.add('active')
+// }
+
+// function closeModal(modal) {
+//     if (modal == null) return
+//     modal.classList.remove('active')
+//     overlay.classList.remove('active')
+// }
+
+const eightTrack = document.querySelector('.eight-track')
+const snipIt = document.querySelector('.snip-it')
+const eightModal = document.querySelector('.eight-modal')
 const overlay = document.getElementById('overlay')
+const closeButton = document.querySelector('.close')
+
+eightTrack.addEventListener('click', openEightModal)
+closeButton.addEventListener('click', closeModal)
+
+function openEightModal() {
+    eightModal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal() {
+    eightModal.classList.remove('active')
+    overlay.classList.remove('active')
+}
