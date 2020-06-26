4be5e75e5795a33aa01a6193be62e6d518fcff0d
MFvanRossum <matissev@hotmail.com>
2020-06-26 14:40:59 -0400

    Snip-It modal functions. Removed close buttons for now. Modals closed by clicking outside of them

--- main.js
+++ main.js
@@ -1,41 +1,13 @@
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-
-// openModalButtons.forEach(div => {
-//     div.addEventListener('click', () => {
-//         const modal = document.querySelector(div.dataset.modalTarget)
-//         openModal(modal)
-//     })
-// })
-
-// closeModalButtons.forEach(div => {
-//     div.addEventListener('click', () => {
-//         const modal = i.closest('.modal')
-//         closeModal(modal)
-//     })
-// })
-
-// function openModal(modal) {
-//     if (modal == null) return
-//     modal.classList.add('active')
-//     overlay.classList.add('active')
-// }
-
-// function closeModal(modal) {
-//     if (modal == null) return
-//     modal.classList.remove('active')
-//     overlay.classList.remove('active')
-// }
-
 const eightTrack = document.querySelector('.eight-track')
 const snipIt = document.querySelector('.snip-it')
 const eightModal = document.querySelector('.eight-modal')
+const snipModal = document.querySelector('.snip-modal')
 const overlay = document.getElementById('overlay')
-const closeButton = document.querySelector('.close')
+// const closeButton = document.querySelector('.close')
 
 eightTrack.addEventListener('click', openEightModal)
-closeButton.addEventListener('click', closeModal)
+snipIt.addEventListener('click', openSnipModal)
+// closeButton.addEventListener('click', closeModal)
 overlay.addEventListener('click', closeModal)
 
 function openEightModal() {
@@ -45,5 +17,11 @@ function openEightModal() {
 
 function closeModal() {
     eightModal.classList.remove('active')
+    snipModal.classList.remove('active')
     overlay.classList.remove('active')
+}
+
+function openSnipModal() {
+    snipModal.classList.add('active')
+    overlay.classList.add('active')
 }
